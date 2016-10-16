f8acccbd53ccab1de0c10fbae55b39904b7636a6
Ubuntu <ubuntu@ip-172-31-61-39.ec2.internal>
2016-10-16 22:26:38 +0000

    nodejs server with static html text

--- helloworld-nodejs/hello_http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http!\n');
-});
-console.log('Server listening on port 8080...');
-server.listen(8080);
